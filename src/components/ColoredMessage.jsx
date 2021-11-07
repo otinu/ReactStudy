d3b1a403afe2170800f484b7abaa3cb77ae9d559
otinu <65097619+otinu@users.noreply.github.com>
2021-11-07 09:07:29 +0900

    [remake]destructure

--- src/components/ColoredMessage.jsx
+++ src/components/ColoredMessage.jsx
@@ -1,11 +1,16 @@
 export const ColoredMessage = (props) => {
-  console.log(props);
+  //Propsを分割代入
+  const { color, children } = props;
+
   //  CSSオブジェクト
   //  JSファイルのオブジェクトにハイフンは使えないため、CSSプロパティ名もキャメルになる
   const contentStyle = {
-    color: props.color,
+    //  color: color, //  分割代入(destructure)により、「props.」を省略
+    //  更に、CSSプロパティ名と変数名が一致したため、省略可能
+    color,
     fontSize: "20px" // 「font-size」ではなく、「fontSize」に注意
   };
 
-  return <p style={contentStyle}>{props.children}</p>;
+  //  分割代入により、「props.」を省略
+  return <p style={contentStyle}>{children}</p>;
 };
