7773ec0cbde7b5f5ab60522154079d5f5a72c797
otinu <65097619+otinu@users.noreply.github.com>
2021-11-07 09:55:47 +0900

    save

--- src/Helloreact.jsx
+++ src/Helloreact.jsx
@@ -1,11 +1,15 @@
 //Railsの命名規則のようなファイル名と関数名を規則に沿って関連付ける必要はない様子
 //ファイル名は「Helloreact」、関数名は「HelloReact」 で動作確認
 
+import { useState } from "react";
 import { ColoredMessage } from "./components/ColoredMessage";
 
 export const HelloReact = () => {
+  const [num, setNum] = useState(0);
+
   const onClickButton = () => {
-    alert();
+    // setNum(num + 1);
+    setNum((prev) => prev + 1); //厳密には、こちらの書き方の方が結合度が低くて良い
   };
 
   //  return以降が複数行になる場合、()で囲う
@@ -17,6 +21,7 @@ export const HelloReact = () => {
       <ColoredMessage color="blue">Feeling difficult but funny</ColoredMessage>
       <ColoredMessage color="pink">Yes! I can do it!!</ColoredMessage>
       <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
     </>
   );
 };
